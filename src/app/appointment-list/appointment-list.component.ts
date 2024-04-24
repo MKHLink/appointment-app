da1cb005624418c4d1d30cc2abdd3d3d71bfa39e
MKHLink <likhonhasankomol@gmail.com>
2024-04-24 16:54:53 -0400

    added data storing and fetching from local storage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css'
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
   
   newAppointmentTitle:string = "";
   newAppointmentDate: Date = new Date();
   
   appointments: Appointment[] = [];
+
+  ngOnInit(): void {
+    let savedApointments = localStorage.getItem("appointments");
+    this.appointments = savedApointments ? JSON.parse(savedApointments) : [];
+  }
+  
 
   addAppointment(){
     if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
@@ -25,10 +32,13 @@ export class AppointmentListComponent {
 
       this.newAppointmentTitle = "";
       this.newAppointmentDate = new Date();
+
+      localStorage.setItem("appointments",JSON.stringify(this.appointments));
     }
   }
 
   deleteAppointment(index: number){
     this.appointments.splice(index, 1);
+    localStorage.setItem("appointments",JSON.stringify(this.appointments));
   }
 }
